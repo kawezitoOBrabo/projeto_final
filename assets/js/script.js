fe794a20c8015267b7b7c1a36b67a9ec1478ddb6
kawezitoOBrabo <129632700+kawezitoOBrabo@users.noreply.github.com>
2023-12-19 09:24:13 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,37 +10,14 @@ function closeMenu(){
   document.querySelector('.slideshow-container').classList.remove('hidden');
 }
 
+window.addEventListener('scroll', function(){
+  let scroll = this.document.querySelector('.topo')
+  scroll.classList.toggle('active', this.window.scrollY > 150)
+})
 
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function backTop(){
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+  })
 }
